177a8283cbcbd3f85340b11b55ae3f96e4eff017
vicrtor villacis <victor.villacis@outlook.com>
2015-11-17 15:13:53 -0500

    added js counter/time

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-// Global Variables
+var counter = 0;
+function countup() {
+  counter = counter + 1;
+  }
 
 
+function countdown() {
+  setTimeout(function countandgive()
+    { 
+    alert("You clicked" +counter+ " Ramaguys!");
+     
+     }, 10000);
 
-  
+}
